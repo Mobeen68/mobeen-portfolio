537bdfd82fdb3d61a838f728b7c175d46026d36c
Mobeen Ejaz <mobeene98@gmail.com>
2024-08-08 01:48:35 +0500

    home page completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,50 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// components
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
+const Home = () => {
+  return (
+    <section className="h-full mb-8">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello I am <br /> <span className="text-accent">Mobeen Ejaz</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting digital experiences and I am proficient in
+              javascript and various technologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                className="uppercase flex items-center gap-2"
+                variant="outline"
+                size="lg"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
+};
+
+export default Home;
