a60a260676803ccd934bd8a7197d3cc75bb05155
Mobeen Ejaz <mobeene98@gmail.com>
2024-08-08 07:01:28 +0500

    site completed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form
               action=""
               className="flex flex-col gap-6 p-10 rounded-xl bg-[#27272c]"
@@ -79,10 +79,31 @@ const Contact = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
+
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here"
+              />
+
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
             </form>
           </div>
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex justify-center items-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
